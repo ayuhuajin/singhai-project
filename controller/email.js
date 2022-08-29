94eeebe1490ef56285d019f670c5e1b6c7bcac50
wangqinghai <455493143@qq.com>
2022-08-29 11:22:20 +0800

    sendNum 添加

--- controller/email.js
+++ controller/email.js
@@ -1,5 +1,4 @@
-const user = require('../mongo/user');
-const jwt = require('jsonwebtoken');
+const company = require('../mongo/company');
 const nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
   "host": "smtpdm.aliyun.com", //SMTP服务地址
@@ -24,8 +23,23 @@ var mailOptions = {
 module.exports={
   sendEmail:async(ctx)=>{
     let data = ctx.request.body;
+    let id = data.companyId
     mailOptions = data
-    console.log(data,12354,mailOptions);
+    // console.log(data,12354,mailOptions);
+    var conditions = {'_id' : id};
+    let result = await company.find(conditions);
+    // console.log(result[0].sendNum,88888999);
+    let sendNum = result[0].sendNum + 1
+    console.log(sendNum,"dddddzjegddd");
+    var update = {$set : {
+      sendNum
+    }};
+    try{
+      await company.update(conditions, update);
+      ctx.response.body = '编辑成功';
+    }catch(err){
+      ctx.response.body='编辑出错';
+    }
     ctx.response.body = '成功';
     return
     transporter.sendMail(mailOptions, function(error, info){
